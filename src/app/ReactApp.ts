a5cda5e324cad0b39f55f207e2ebc83ab00b6642
Garry Xiao <xz@etsoo.com>
2021-04-05 15:35:14 +1200

    Add changeLanguageEx

--- src/app/ReactApp.ts
+++ src/app/ReactApp.ts
@@ -1,4 +1,7 @@
 import { CoreApp, IAppSettings } from '@etsoo/appscript';
+import { DataTypes } from '@etsoo/shared';
+import React from 'react';
+import { LanguageAction } from '../states/LanguageState';
 
 /**
  * React application
@@ -6,4 +9,21 @@ import { CoreApp, IAppSettings } from '@etsoo/appscript';
 export abstract class ReactApp<S extends IAppSettings> extends CoreApp<
     S,
     React.ReactNode
-> {}
+> {
+    /**
+     * Change language extended
+     * @param dispatch Dispatch method
+     * @param language New lnguage definition
+     */
+    changeLanguageEx(
+        dispatch: React.Dispatch<LanguageAction>,
+        language: DataTypes.LanguageDefinition
+    ): void {
+        // Dispatch action
+        const action: LanguageAction = language;
+        dispatch(action);
+
+        // Super call
+        super.changeLanguage(language);
+    }
+}
