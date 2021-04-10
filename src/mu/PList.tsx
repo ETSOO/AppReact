44f75cf8d7a1c7f80e5ce97618cf685e077aaeb9
Garry Xiao <xz@etsoo.com>
2021-04-10 20:12:45 +1200

    Improve PList

--- src/mu/PList.tsx
+++ src/mu/PList.tsx
@@ -5,9 +5,12 @@ import React from 'react';
  * @param items Items
  */
 export function PList(items?: string[]) {
-    if (items == null || items.length == 0) return <React.Fragment />;
-
-    return items.map((item, index) => {
-        return <p key={index}>{item}</p>;
-    });
+    return (
+        <React.Fragment>
+            {items != null &&
+                items.map((item, index) => {
+                    return <p key={index}>{item}</p>;
+                })}
+        </React.Fragment>
+    );
 }
