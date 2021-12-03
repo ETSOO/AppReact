bef05c82be4b5d0203e8dc4523e37c6796466606
Garry Xiao <xz@etsoo.com>
2021-12-04 12:50:46 +1300

    Add tryLogin to ServiceApp

--- src/app/ServiceApp.ts
+++ src/app/ServiceApp.ts
@@ -1,10 +1,18 @@
-import { createClient, IApi } from '@etsoo/appscript';
+import {
+    createClient,
+    IActionResult,
+    IApi,
+    IApiPayload
+} from '@etsoo/appscript';
+import { ApiDataError } from '@etsoo/restclient';
 import { DomUtils } from '@etsoo/shared';
-import { ISmartERPUser } from '..';
+import { CoreConstants } from './CoreConstants';
 import { IServiceAppSettings } from './IServiceAppSettings';
 import { IServicePageData } from './IServicePage';
-import { IServiceUser } from './IServiceUser';
+import { IServiceUser, ServiceLoginResult } from './IServiceUser';
+import { ISmartERPUser, SmartERPLoginResult } from './ISmartERPUser';
 import { ReactApp } from './ReactApp';
+import { RefreshTokenRQ } from './RefreshTokenRQ';
 
 /**
  * Core Service App
@@ -61,6 +69,96 @@ export class ServiceApp<
             DomUtils.Culture +
             '=' +
             this.culture;
+    }
+
+    /**
+     * Try login result processing
+     * @param result Result
+     */
+    protected tryLoginResult(result: string | ApiDataError | IActionResult) {}
+
+    /**
+     * Try login
+     */
+    override async tryLogin() {
+        // Token
+        const token = this.getCacheToken();
+        if (token == null || token === '') {
+            this.tryLoginResult('');
+            return false;
+        }
+
+        // Reqest data
+        const data: RefreshTokenRQ = {
+            timezone: this.getTimeZone()
+        };
+
+        // Payload
+        const payload: IApiPayload<SmartERPLoginResult, any> = {
+            config: { headers: { [CoreConstants.TokenHeaderRefresh]: token } },
+            onError: (error) => {
+                this.tryLoginResult(error);
+
+                // Prevent further processing
+                return false;
+            }
+        };
+
+        // Call API
+        const result = await this.coreApi.put<SmartERPLoginResult>(
+            'Auth/RefreshToken',
+            data,
+            payload
+        );
+        if (result == null) return false;
+
+        if (!result.ok) {
+            this.tryLoginResult(result);
+            return false;
+        }
+
+        // Token
+        const refreshToken = this.getResponseToken(payload.response);
+        if (refreshToken == null || result.data == null) {
+            this.tryLoginResult(this.get('noData')!);
+            return false;
+        }
+
+        // User data
+        const userData = result.data;
+
+        // Use core system access token to service api to exchange service access token
+        const serviceResult = await this.api.put<ServiceLoginResult>(
+            'Auth/ExchangeToken',
+            {
+                token: userData.token
+            },
+            {
+                onError: (error) => {
+                    // Set message
+                    this.tryLoginResult(error);
+
+                    // Prevent further processing
+                    return false;
+                }
+            }
+        );
+
+        if (serviceResult == null) return false;
+
+        if (!serviceResult.ok) {
+            this.tryLoginResult(result);
+            return false;
+        }
+
+        if (serviceResult.data == null) {
+            this.tryLoginResult(this.get('noData')!);
+            return false;
+        }
+
+        this.userLoginEx(serviceResult.data, refreshToken, userData);
+
+        return true;
     }
 
     /**
