84d6ba2024007f05f3486bc21e2524d1e7cf8dad
Garry Xiao <xz@etsoo.com>
2022-02-12 22:48:00 +1300

    Fix createMemorySource navigate(-1) bug

--- src/app/ReactUtils.ts
+++ src/app/ReactUtils.ts
@@ -1,10 +1,13 @@
-import {
-    createHistory,
-    createMemorySource,
-    History,
-    navigate
-} from '@reach/router';
+import { createHistory, History, HistorySource, navigate } from '@reach/router';
 import React from 'react';
+
+type MemoryHistorySource = HistorySource & {
+    history: {
+        get entries(): any;
+        get index(): number;
+        go(to: number): void;
+    };
+};
 
 /**
  * React utils
@@ -13,6 +16,82 @@ export namespace ReactUtils {
     // Memory history
     // https://github.com/reach/router/issues/225
     let memoryHistory: History | null;
+
+    /**
+     * Cretae memory source to fix go back bug
+     * @param initialPath Initial path
+     */
+    export function createMemorySource(
+        initialPath: string = '/'
+    ): MemoryHistorySource {
+        const searchIndex = initialPath.indexOf('?');
+        const pathname =
+            searchIndex > -1
+                ? initialPath.substring(0, searchIndex)
+                : initialPath;
+
+        const initialLocation: any = {
+            pathname,
+            search: searchIndex > -1 ? initialPath.substring(searchIndex) : ''
+        };
+
+        let index = 0;
+
+        const stack = [initialLocation];
+        const states = [null];
+
+        return {
+            get location() {
+                return stack[index];
+            },
+            addEventListener(name, fn) {},
+            removeEventListener(name, fn) {},
+            history: {
+                get entries() {
+                    return stack;
+                },
+                get index() {
+                    return index;
+                },
+                get state() {
+                    return states[index];
+                },
+                pushState(state, _, uri) {
+                    const [pathname, search = ''] = uri.split('?');
+                    index++;
+
+                    // View a, index = 0
+                    // View b, index = 1
+                    // Go(-1), a, index = 0
+                    // View c, index = 1, directly push is wrong
+                    if (index < stack.length) {
+                        stack.splice(index, stack.length - index);
+                        states.splice(index, stack.length - index);
+                    }
+
+                    stack.push({
+                        pathname,
+                        search: search.length ? `?${search}` : search
+                    });
+                    states.push(state);
+                },
+                replaceState(state, _, uri) {
+                    const [pathname, search = ''] = uri.split('?');
+                    stack[index] = { pathname, search };
+                    states[index] = state;
+                },
+                go(to: number) {
+                    const newIndex = index + to;
+
+                    if (newIndex < 0 || newIndex > states.length - 1) {
+                        return;
+                    }
+
+                    index = newIndex;
+                }
+            }
+        };
+    }
 
     /**
      * Format input value
@@ -50,13 +129,6 @@ export namespace ReactUtils {
      * @returns NavigateFn
      */
     export function getNavigateFn() {
-        console.log(
-            'getNavigateFn',
-            memoryHistory,
-            memoryHistory?.navigate,
-            memoryHistory?.navigate === navigate
-        );
-
         if (memoryHistory == null) return navigate;
         return memoryHistory.navigate;
     }
