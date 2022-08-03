bd94c14bfa1f9235b9bf62a87eb5ff764f7f0742
Garry Xiao <xz@etsoo.com>
2022-08-03 22:49:06 +1200

    Remove DnDList

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,6 @@ export * from './mu/CustomFabProps';
 export * from './mu/DataGridEx';
 export * from './mu/DataGridRenderers';
 export * from './mu/DialogButton';
-export * from './mu/DnDList';
 export * from './mu/DraggablePaperComponent';
 export * from './mu/EmailInput';
 export * from './mu/FabBox';
