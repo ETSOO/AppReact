da902a8367aa997971ea43420340aebc94b75757
Garry Xiao <xz@etsoo.com>
2022-09-21 11:53:45 +1200

    Add EventWatcher

---
+++ __tests__/EventWatcher.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { EventWatcher } from '../src/app/EventWatcher';
+
+function App(props: { callback: () => void }) {
+    const { callback } = props;
+    const watcher = new EventWatcher();
+
+    React.useEffect(() => {
+        watcher.add({
+            type: 'click',
+            action: (_event) => callback(),
+            once: true
+        });
+    }, []);
+    return <button onClick={watcher.do}></button>;
+}
+
+test('Tests for EventWatcher', () => {
+    const callback = jest.fn();
+    render(<App callback={callback} />);
+    const button = screen.getByRole<HTMLButtonElement>('button');
+    button.click();
+    expect(callback).toBeCalled();
+    button.click();
+    expect(callback).toBeCalledTimes(1);
+});
---
+++ src/app/EventWatcher.ts
@@ -0,0 +1,66 @@
+import React from 'react';
+
+/**
+ * Event watcher action
+ */
+export interface EventWatcherAction {
+    /**
+     * Event type
+     */
+    type?: string | string[];
+
+    /**
+     * Once action or not
+     */
+    once?: boolean;
+
+    /**
+     * Action
+     */
+    action: (
+        event: React.BaseSyntheticEvent | string
+    ) => void | PromiseLike<void>;
+}
+
+/**
+ * Event watcher
+ */
+export class EventWatcher {
+    private actions: EventWatcherAction[] = [];
+
+    /**
+     * Add action
+     * @param action Action
+     */
+    add(action: EventWatcherAction) {
+        this.actions.push(action);
+    }
+
+    /**
+     * Do the event
+     * @param event Event
+     */
+    do(event: React.BaseSyntheticEvent | string) {
+        // Type
+        const type = typeof event === 'string' ? event : event.type;
+
+        // Execute
+        const removeIndices: number[] = [];
+        this.actions.forEach((item, index) => {
+            if (!this.isMatch(item, type)) return;
+            item.action(event);
+            if (item.once) removeIndices.push(index);
+        });
+
+        // Remove all once actions
+        removeIndices
+            .reverse()
+            .forEach((index) => this.actions.splice(index, 1));
+    }
+
+    private isMatch(action: EventWatcherAction, type: string) {
+        if (action.type == null) return true;
+        if (typeof action.type === 'string') return action.type === type;
+        return action.type.includes(type);
+    }
+}
