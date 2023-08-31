cd521a3a3715b2c3d770724235f60029fe169e3c
Garry Xiao <xz@etsoo.com>
2023-08-31 21:54:21 +1200

    Improve ListItemReact

--- src/components/ListItemReact.ts
+++ src/components/ListItemReact.ts
@@ -4,4 +4,4 @@ import React from 'react';
 /**
  * React list item
  */
-export type ListItemReact = ListItem<React.ReactNode>;
+export type ListItemReact = ListItem<React.ReactNode, React.MouseEvent>;
