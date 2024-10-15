6ac06e762200ded4df3337f74a055914ac288e9c
Garry Xiao <xz@etsoo.com>
2024-10-15 18:26:49 +1300

    Remove TokenHeaderRefresh

--- src/app/CoreConstants.ts
+++ src/app/CoreConstants.ts
@@ -16,9 +16,4 @@ export namespace CoreConstants {
      * Rresh token cache field
      */
     export const FieldRefreshToken = 'RefreshToken';
-
-    /**
-     * Refresh token header field name
-     */
-    export const TokenHeaderRefresh = 'Smarterp-Refresh-Token';
 }
