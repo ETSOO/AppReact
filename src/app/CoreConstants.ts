6f808685dee7178298f207fa5732d4ba70927d22
Garry Xiao <xz@etsoo.com>
2024-10-15 23:11:34 +1300

    Upgrade dependencies

--- src/app/CoreConstants.ts
+++ src/app/CoreConstants.ts
@@ -3,11 +3,6 @@
  */
 export namespace CoreConstants {
     /**
-     * Login keep field name
-     */
-    export const FieldLoginKeep = 'LoginKeep';
-
-    /**
      * User id saved field
      */
     export const FieldUserIdSaved = 'UserIdSaved';
