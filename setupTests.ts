d8c7764e752a4c00816ce07bd3b9a2cbae183321
Garry Xiao <xz@etsoo.com>
2025-11-05 07:49:26 +0800

    Upgrade dependencies

--- setupTests.ts
+++ setupTests.ts
@@ -1,13 +1,7 @@
+import { MockResizeObserver } from "@etsoo/shared";
 import { vi } from "vitest";
 
 (globalThis as any).IS_REACT_ACT_ENVIRONMENT = true;
 
-// Mock the ResizeObserver
-const ResizeObserverMock = vi.fn(() => ({
-  observe: vi.fn(),
-  unobserve: vi.fn(),
-  disconnect: vi.fn()
-}));
-
 // Stub the global ResizeObserver
-vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+vi.stubGlobal("ResizeObserver", MockResizeObserver);
